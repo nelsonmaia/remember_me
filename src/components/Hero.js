67c9b8ca4e06bb3d81d7bc95dcc5036ddc496890
nellson.maia@gmail.com <nellson.maia@gmail.com>
2021-10-11 13:24:07 +0100

    make it better

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,7 +4,7 @@ import logo from "../assets/logo.svg";
 
 const Hero = () => (
   <div className="">
-    <img src="http://localhost:3000/ikea-bottom.png"  id="bg" alt=""></img>
+    <img src="ikea-bottom.png"  id="bg" alt=""></img>
   </div>
 );
 
